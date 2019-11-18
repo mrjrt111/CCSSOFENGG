9d9c99762f0416a960e870ed6d5ecb851577308d
Losinek <43725421+Losinek@users.noreply.github.com>
2019-11-18 14:23:09 +0800

    edited mod officer and moved it

---
+++ FrontEnd/Revised register, login, document view, mod officer/assets/js/buttonInteractionMO.js
@@ -0,0 +1,12 @@
+$("#editButt").click(function(){
+  $("#editOfficerModal").modal("show");
+  });
+
+$("#deleteButt").click(function(){
+  $("#deleteOfficerModal").modal("show");
+  });
+
+
+$("#blacklistWhitelistButt").click(function(){
+  $("#blacklistWhitelistOfficerModal").modal("show");
+  });
