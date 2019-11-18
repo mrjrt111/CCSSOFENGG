789c4339c299f51cfa5b12263d5ddd10834d3b15
Losinek <43725421+Losinek@users.noreply.github.com>
2019-11-18 13:31:52 +0800

    whoops i forgot to push it

---
+++ FrontEnd/Revised Encode/assets/js/hiding form div.js
@@ -0,0 +1,8 @@
+//to make preacts visible
+
+// $('#encodePreactsForm').removeClass('hidden');
+// $('#encodePreactsForm').addClass('flex');
+
+//to make postacts visible
+// $('#encodePostactsForm').removeClass('hidden');
+// $('#encodePostactsForm').addClass('flex');
