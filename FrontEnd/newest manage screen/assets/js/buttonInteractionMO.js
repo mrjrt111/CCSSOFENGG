8872391c43094fefcae44532346e92b987020f08
Losinek <43725421+Losinek@users.noreply.github.com>
2019-11-20 18:47:36 +0800

    newest one

---
+++ FrontEnd/newest manage screen/assets/js/buttonInteractionMO.js
@@ -0,0 +1,22 @@
+$("#editButt").click(function(){
+  $("#editOfficerModal").modal("show");
+  });
+
+$("#deleteButt").click(function(){
+  $("#deleteOfficerModal").modal("show");
+  });
+
+
+$("#blacklistWhitelistButt").click(function(){
+  $("#blacklistWhitelistOfficerModal").modal("show");
+  });
+
+$("#deleteOrgButt").click(function(){
+   $("#deleteOrgModal").modal("show"); 
+});
+$("#blacklistOrgButt").click(function(){
+   $("#blacklistOrgModal").modal("show"); 
+});
+$("#deleteOrgPromptButt").click(function(){
+   $("#deleteOrgPromptModal").modal("show"); 
+});
